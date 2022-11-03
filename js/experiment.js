1e85bd3c2e5693032c18c80535693f39d162cd1e
adkipnis <adkipnis@mailbox.org>
2022-11-03 18:31:20 +0100

    Consent form

--- js/experiment.js
+++ js/experiment.js
@@ -2,7 +2,7 @@
 // let jsPsych = initJsPsych();
 const timestamp = getTimeStamp();
 
-
+// Helpers
 function twoPad(n) {
     return String(n).padStart(2, '0');
 }
@@ -17,6 +17,30 @@ function getTimeStamp() {
     let second = date.getSeconds();
 
     return [year, twoPad(month), twoPad(day), twoPad(hour), twoPad(minute), twoPad(second)].join('-');
+}
+
+// Before Experiment
+function changePage(hide, show) {
+    document.getElementById(hide).style.display = "none";
+    document.getElementById(show).style.display = "block";
+    window.scrollTo(0, 0);
+}
+
+function openFullscreen(elem, testing = false) {
+    if (testing) return
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
+
+function loadConsent() {
+    changePage("page_welcome", "page_consent");
+    // let elem = document.body;
+    // openFullscreen(elem);
 }
 
 // Testing
