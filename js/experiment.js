0decbcee39e1aa8976e496e7097a55cce93ab4f2
adkipnis <adkipnis@mailbox.org>
2022-11-17 16:16:33 +0100

    generate options for age and nationality via js

--- js/experiment.js
+++ js/experiment.js
@@ -4,6 +4,201 @@ let fs = true;
 let sliderMoved = false;
 let trialNum = 0;
 
+let nats = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "American Samoa",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antarctica",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas (the)",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bermuda",
+    "Bhutan",
+    "Bolivia",
+    "Bonaire",
+    "Bosnia",
+    "Botswana",
+    "Brazil",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi",
+    "Cambodia",
+    "Cameroon",
+    "Canada",
+    "Central African Republic",
+    "Chad",
+    "Chile",
+    "China",
+    "Christmas Island",
+    "Colombia",
+    "Congo",
+    "Costa Rica",
+    "Croatia",
+    "Cuba",
+    "Curaçao",
+    "Cyprus",
+    "Czechia",
+    "Côte d'Ivoire",
+    "Denmark",
+    "Djibouti",
+    "Dominican Republic",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Eritrea",
+    "Estonia",
+    "Ethiopia",
+    "Finland",
+    "France",
+    "Gabon",
+    "Gambia",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Gibraltar",
+    "Greece",
+    "Greenland",
+    "Grenada",
+    "Guadeloupe",
+    "Guatemala",
+    "Guinea",
+    "Guinea-Bissau",
+    "Guyana",
+    "Haiti",
+    "Herzegovina",
+    "Honduras",
+    "Hong Kong",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran",
+    "Iraq",
+    "Ireland",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jersey",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Korea",
+    "Kuwait",
+    "Kyrgyzstan",
+    "Laos",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Mauritania",
+    "Mauritius",
+    "Mexico",
+    "Micronesia",
+    "Moldova",
+    "Monaco",
+    "Mongolia",
+    "Montenegro",
+    "Montserrat",
+    "Morocco",
+    "Mozambique",
+    "Myanmar",
+    "Namibia",
+    "Nepal",
+    "Netherlands",
+    "New Caledonia",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palau",
+    "Palestine",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Poland",
+    "Portugal",
+    "Puerto Rico",
+    "Qatar",
+    "Republic of North Macedonia",
+    "Romania",
+    "Russian Federation",
+    "Rwanda",
+    "Samoa",
+    "San Marino",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Slovakia",
+    "Slovenia",
+    "Somalia",
+    "South Africa",
+    "South Sudan",
+    "Spain",
+    "Sri Lanka",
+    "Sudan",
+    "Sweden",
+    "Switzerland",
+    "Syria",
+    "Taiwan",
+    "Tajikistan",
+    "Tanzania",
+    "Thailand",
+    "Togo",
+    "Tonga",
+    "Trinidad",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Uganda",
+    "Ukraine",
+    "UAE",
+    "UK",
+    "USA",
+    "Uruguay",
+    "Uzbekistan",
+    "Venezuela",
+    "Vietnam",
+    "Western Sahara",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe"]
+
 // Data
 function randomID(length = 32) {
     let result = "";
@@ -213,11 +408,30 @@ function loadMain() {
     });
 }
 
+function fillSelect(id, list) {
+    let sel = document.getElementById(id);
+    for (let el of list) {
+        let opt = document.createElement("option");
+        opt.value = el;
+        opt.innerHTML = el;
+        sel.appendChild(opt);
+    }
+}
+
+function fillAge() {
+    let ages = Array.from(Array(100).keys()).filter(i => i > 17);
+    fillSelect("age", ages);
+}
+fillAge();
+
+function fillNationalities() {
+    fillSelect("nationality", nats);
+}
+fillNationalities();
 
 function loadEnd() {
     let checkq1 = document.querySelector('input[name="q1"]:checked');
     let checkq2 = document.getElementById('age').value;
-    let checkq2R = new RegExp(document.getElementById('age').pattern);
     let checkq3 = document.getElementById('nationality').value;
 
     if (checkq1 == null || checkq2 == "" || checkq3 == "") {
@@ -226,15 +440,7 @@ function loadEnd() {
             icon: "warning",
             buttons: ["OK", false]
         });
-    }
-    else if (!checkq2R.test(checkq2)) {
-        swal({
-            text: "Your age is in the wrong format!",
-            icon: "warning",
-            buttons: ["OK", false]
-        });
-    }
-    else {
+    } else {
         metadata["gender"] = checkq1.value;
         metadata["age"] = checkq2;
         metadata["nationality"] = checkq3;
