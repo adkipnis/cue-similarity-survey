ce6648a48a9060829c6b9bb63dbcccdcf1015ffe
adkipnis <adkipnis@mailbox.org>
2022-11-21 11:45:57 +0100

    aggregate data and filter out bad trials

--- data/analysis.R
+++ data/analysis.R
@@ -2,40 +2,49 @@
 #             Cue Similarity Analysis            #
 #================================================#
 
-# === Preprocessing ===
+# ==== Preprocessing ===========================================================
 # --- Clean Up
 rm(list = ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # --- Load data
 fnames = list.files(pattern = "data", recursive = T, full.names = T)
-df = data.frame()
+full = data.frame()
 meta = data.frame()
 for (fname in fnames){
   json_list = jsonlite::fromJSON(fname)
   tmp =  as.data.frame(do.call("cbind", json_list))
-  df = dplyr::bind_rows(df, tmp)
+  full = dplyr::bind_rows(full, tmp)
   meta = dplyr::bind_rows(meta, tail(tmp, n = 1))
 }
 
 # --- Infer Durations
 format = "%Y-%m-%d %H:%M:%OS"
-df$rt = as.numeric(difftime(strptime(df$trial_end, format),
-                            strptime(df$trial_start, format))) 
-meta$duration = as.numeric(difftime(strptime(meta$trial_end, format),
+full$rt = as.numeric(difftime(strptime(full$trial_end, format),
+                            strptime(full$trial_start, format))) 
+meta$total_duration = as.numeric(difftime(strptime(meta$trial_end, format),
                                     strptime(meta$start, format))) 
 
 # --- Remove irrelevant info
-trials = subset(df, select=c(subject_id, stim_left, stim_right, similarity, rt))
+trials = subset(full, select=c(subject_id, stim_left, stim_right, similarity, rt))
 metadata = subset(meta, select=-c(
-  start, stim_left, stim_right, similarity, trial_start, trial_end, end_time,
-  survey_time))
+  start, stim_left, stim_right, similarity,
+  trial_start, trial_end, end_time, survey_time))
+rm(json_list, tmp, meta)
+
+# --- Save aggregated data
+write.csv(trials,"./trials.csv", row.names = F)
+write.csv(metadata,"./participant_info.csv", row.names = F)
 
 # --- Discard trials / participants
-# TODO min reaction time, min duration
+min_total_duration = 10 # number of trials x 1s
+min_trial_duration = 0.5 # in seconds
+good_subjects = metadata$subject_id[metadata$total_duration >= min_total_duration]
+trials = trials[trials$subject_id %in% good_subjects,]
+trials = trials[trials$rt >= min_trial_duration,]
 
 
-# === Main Analysis ===
+# ==== Main Analysis ===========================================================
 # --- Retest Reliability
 # TODO
 
